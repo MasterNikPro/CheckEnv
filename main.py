91526859887c78f4ca2506a13cec2603d69e8bcb
Nikita <86921169+MasterNikPro@users.noreply.github.com>
2025-04-02 01:06:55 +0300

    ENV-2: Add default variable checking

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hi, from checkenv project")
+import os
+import sys
+
+DEFAULT_VARS = ["DB_HOST", "API_KEY", "SECRET_KEY"]
+
+def check_env_vars(vars_to_check):
+    missing = []
+    for var in vars_to_check:
+        if not os.getenv(var):
+            missing.append(var)
+    return missing
